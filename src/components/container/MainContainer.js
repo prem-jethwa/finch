8b656865ba0fc748e7ac180fe47209263aaaf0ff
premjethwa <prem.jethwa@viamaguspro.com>
2022-03-16 20:18:32 +0530

    Responsive for card, MainCon, filter added

--- src/components/container/MainContainer.js
+++ src/components/container/MainContainer.js
@@ -6,10 +6,15 @@ import { useEffect, useState } from "react";
 
 function MainContainer({ onFilterChange }) {
   const [filteredObj, setFilteredObj] = useState({});
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
 
   useEffect(() => {
     onFilterChange(filteredObj);
   }, [filteredObj]);
+
+  useEffect(() => {
+    window.innerWidth < 768 && setIsFilterOpen(false);
+  }, [window.innerWidth > 768]);
 
   const handleFilterChange = (e) => {
     setFilteredObj({ ...filteredObj, ...e });
@@ -17,12 +22,20 @@ function MainContainer({ onFilterChange }) {
 
   return (
     <div className={classes["main-container"]}>
-      <FilterSideBar
-        minPriceRange={200}
-        maxPriceRange={800}
-        shoeSizes={[30, 40, 50]}
-        onFilterSideBarChange={handleFilterChange}
-      />
+      {window.innerWidth > 768 || isFilterOpen ? (
+        <FilterSideBar
+          minPriceRange={200}
+          maxPriceRange={800}
+          shoeSizes={[30, 40, 50]}
+          onFilterSideBarChange={handleFilterChange}
+        />
+      ) : null}
+      <div
+        className={classes["main-container--filter-dropdown"]}
+        onClick={() => setIsFilterOpen(!isFilterOpen)}
+      >
+        {!isFilterOpen ? "Open" : "Close"} Filter
+      </div>
       <MainSection onSortByPriceChange={handleFilterChange} />
     </div>
   );
